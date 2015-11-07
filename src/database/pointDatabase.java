945ecf43afaecfb6c008c067c88ed7777f66f61e
Arkessler <Arkessler@wpi.edu>
2015-11-07 00:30:22 -0500

    Added some general functionality to database

--- src/database/pointDatabase.java
+++ src/database/pointDatabase.java
@@ -7,29 +7,27 @@ import java.sql.Statement;
 
 public class pointDatabase 
 {
+	private static String DatabaseName = "pointDatabase.db";
+	private static String PointSchema = "id integer, xCoord integer, yCoord integer, name String";
 	private static Connection connection;
 	
 	public static void main(String args[]) throws ClassNotFoundException
 	{
 		Class.forName("org.sqlite.JDBC");											//Look into exactly what this is intended to do
-		connection = null;
+		connection = null;															//Initialize connection
 		try
-		{
-																					// create a database connection
-			connection = DriverManager.getConnection("jdbc:sqlite:pointDatabase.db");
+		{							
+			connection = DriverManager.getConnection("jdbc:sqlite:"+DatabaseName);	// create a database connection
 			Statement statement = connection.createStatement();
 			statement.setQueryTimeout(30);  										// set timeout to 30 sec.
 
-			statement.executeUpdate("drop table if exists person");
-			statement.executeUpdate("create table person (id integer, name string)");
-			statement.executeUpdate("insert into person values(1, 'Tyrome')");
-			statement.executeUpdate("insert into person values(2, 'Paula')");
-			ResultSet rs = statement.executeQuery("select * from person");
-			while(rs.next())														// read the result set
-			{
-				System.out.println("name = " + rs.getString("name"));
-				System.out.println("id = " + rs.getInt("id"));
-			}
+			statement.executeUpdate("drop table if exists points");
+			statement.executeUpdate("create table points ("+ PointSchema + ")");
+			statement.executeUpdate("insert into points values(1, 23, 45, 'SL105')");
+			statement.executeUpdate("insert into points values(2, 46, 98, 'AK206')");
+			statement.executeUpdate("insert into points values(3, 90, 12, 'FL120')");
+			statement.executeUpdate("insert into points values(4, 16, 25, 'KV121')");
+			printDatabase();
 		}
 		catch(SQLException e)
 		{
@@ -50,4 +48,32 @@ public class pointDatabase
 			}
 		}
 	}
+	
+	public void insertPoint()
+	{
+		
+	}
+	
+	public static void printDatabase()
+	{
+		try
+		{
+			Statement statement = connection.createStatement();
+			ResultSet rs = statement.executeQuery("select * from points");
+			while(rs.next())														// read the result set
+			{
+				System.out.println("name = " + rs.getString("name"));
+				System.out.println("id = " + rs.getInt("id"));
+				System.out.print("xCoord = " + rs.getInt("xCoord"));
+				System.out.println(", yCoord = " + rs.getInt("yCoord"));
+				System.out.println();
+			}
+		}
+		catch(SQLException e)
+		{
+			System.err.println(e.getMessage());										// if the error message is "out of memory", 
+																					// it probably means no database file is found
+		}
+	}
+
 }	

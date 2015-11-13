cf0f311e4273eb7999bf24cf2ad9fdc1d86d55db
Arkessler <Arkessler@wpi.edu>
2015-11-13 14:26:11 -0500

    Added more to point and edge, changed Astar to work with them

--- src/main_package/Edge.java
+++ src/main_package/Edge.java
@@ -1,15 +1,63 @@
 package main_package;
 
 public class Edge {
-	public Point a;
-	public Point b;
-	public int weight;
+	private int id;
+	private Point point1;
+	private Point point2;
+	private int weight;
+	private boolean isOutside;
+	private boolean isStairs;
 	
-	public Edge(Point ea, Point eb, int eweight){
-		a = ea;
-		b = eb;
-		weight = eweight;
-		a.addEdge(this);
-		b.addEdge(this);
+	public Edge(Point point1, Point point2, int weight){
+		this.point1 = point1;
+		this.point2 = point2;
+		this.weight = weight;
+		point1.addEdge(this);
+		point2.addEdge(this);
 	}
+	public int getID()
+	{
+		return id;
+	}
+	public void setID(int newID)
+	{
+		this.id = newID;
+	}
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public Point getPoint1() {
+		return point1;
+	}
+	public void setPoint1(Point point1) {
+		this.point1 = point1;
+	}
+	public Point getPoint2() {
+		return point2;
+	}
+	public void setPoint2(Point point2) {
+		this.point2 = point2;
+	}
+	public int getWeight() {
+		return weight;
+	}
+	public void setWeight(int weight) {
+		this.weight = weight;
+	}
+	public boolean isOutside() {
+		return isOutside;
+	}
+	public void setOutside(boolean isOutside) {
+		this.isOutside = isOutside;
+	}
+	public boolean isStairs() {
+		return isStairs;
+	}
+	public void setStairs(boolean isStairs) {
+		this.isStairs = isStairs;
+	}
+	
 }

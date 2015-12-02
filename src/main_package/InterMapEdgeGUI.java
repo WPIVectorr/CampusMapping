a412537282c206b03873a6d5d5000638caa4f36f
graffjosh <Github2@joshuagraff.com>
2015-12-01 19:21:48 -0500

    Fixed Conflicts??

--- src/main_package/InterMapEdgeGUI.java
+++ src/main_package/InterMapEdgeGUI.java
@@ -2,27 +2,16 @@ package main_package;
 
 
 import javax.swing.JFrame;
-import javax.swing.JSplitPane;
 import javax.swing.UIManager;
 import javax.swing.UnsupportedLookAndFeelException;
 
-import database.DoesNotExistException;
 import database.ServerDB;
-import main_package.MapInserterGUI.PaintFrame;
-//import main_package.MapUpdaterGUI.UpdateMap;
-
 import java.awt.BorderLayout;
-import java.awt.Color;
 import java.awt.Dimension;
 import java.awt.Graphics;
-import java.awt.Graphics2D;
-
 import javax.swing.JLabel;
-import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 
-import java.awt.Panel;
-import java.awt.RenderingHints;
 import java.awt.Toolkit;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
@@ -30,18 +19,12 @@ import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
 import java.awt.image.BufferedImage;
 import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileOutputStream;
 import java.io.IOException;
 import java.util.ArrayList;
 
 import javax.imageio.ImageIO;
 import javax.swing.JButton;
 import javax.swing.JComboBox;
-import javax.swing.JToolBar;
-import javax.swing.BoxLayout;
-import javax.swing.JInternalFrame;
-import java.awt.FlowLayout;
 import java.awt.GridBagLayout;
 import java.awt.GridBagConstraints;
 import java.awt.Insets;
@@ -71,7 +54,7 @@ public class InterMapEdgeGUI extends JFrame {
 	String name;
 	File destinationFile;
 	File logo;
-
+ 
 	private Map currentMap = null;
 	private ServerDB md = ServerDB.getInstance();
 
--- src/main_package/MapInserterGUIButtonPanel.java
+++ src/main_package/MapInserterGUIButtonPanel.java
@@ -1,34 +1,19 @@
 package main_package;
 
 import javax.swing.JFrame;
-import javax.swing.JSplitPane;
 import javax.swing.UIManager;
 import javax.swing.UnsupportedLookAndFeelException;
 
-import main_package.MapInserterGUI.PaintFrame;
-//import main_package.MapUpdaterGUI.UpdateMap;
-
-import java.awt.BorderLayout;
 import java.awt.Dimension;
 import java.awt.Graphics;
-import java.awt.Graphics2D;
-
 import javax.swing.JLabel;
-import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 
-import java.awt.Panel;
-import java.awt.RenderingHints;
-import java.awt.Toolkit;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import java.awt.event.MouseAdapter;
-import java.awt.event.MouseEvent;
 import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
-import java.util.ArrayList;
-
 import javax.imageio.ImageIO;
 import javax.swing.JButton;
 import javax.swing.JComboBox;

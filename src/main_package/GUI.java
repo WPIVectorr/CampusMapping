0cccc0f06fb17df84405258388b7b0bdd887052f
Brian <blrubenstein@wpi.edu>
2015-12-16 00:12:48 -0500

    Gui change?

--- src/main_package/GUI.java
+++ src/main_package/GUI.java
@@ -407,7 +407,9 @@ public class GUI implements Runnable{
 			public void actionPerformed(ActionEvent e) {
 				menuLayout.show(menus, "Pref Menu");
 				img = tempImg;
-				roomPointsToDraw = getRoomPoints(currentMap.getPointList());
+				if(currentMap != null){
+					roomPointsToDraw = getRoomPoints(currentMap.getPointList());
+				}
 				if(isShowStart){
 					startIsSelected = true;
 					isShowStart = false;
@@ -2433,7 +2435,9 @@ public class GUI implements Runnable{
 				panelHelp.setVisible(false);
 				menus.setVisible(true);
 				panelLayout.show(panels, "Draw Panel");
-				roomPointsToDraw = getRoomPoints(currentMap.getPoints());
+				if(currentMap != null){
+					roomPointsToDraw = getRoomPoints(currentMap.getPoints());
+				}
 			}
 		});
 
